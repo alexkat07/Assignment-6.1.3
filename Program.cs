9648ac0f39cf567a74813bc5d08ba69409ebf1ea
alexk <alexk@DESKTOP-QL7PNT4>
2024-06-10 18:50:33 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Assignment_6._1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] nums = [0, 1, 0, 3, 12];
+
+            int count = 0;
+            
+            for (int i = 0; i < nums.Length; i++) 
+            { 
+                if (nums[i] != 0) 
+                {
+                    nums[count] = nums[i];
+                    count++;
+                }
+            }
+            while (count < nums.Length) 
+            {
+                nums[count] = 0;
+                count++;
+            }
+            for (int i = 0; i < nums.Length; ++i) 
+            {
+                Console.Write(nums[i]+ " ");
+            }
+        }
+       
+    }
+}
